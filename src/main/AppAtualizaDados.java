3d9246bd906ef9fd20a8c27ba9860df5a9155188
George Henrique <georgehenriquecampos@gmail.com>
2021-01-19 00:44:36 -0300

    Programa para atualizar dados do banco.

---
+++ src/main/AppAtualizaDados.java
@@ -0,0 +1,27 @@
+package main;
+
+/*import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB; */
+
+public class AppAtualizaDados {
+
+	public static void main(String[] args) {
+		/* Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"UPDATE titulos SET lancamento = lacamento + ? WHERE ()" 
+				    + PreparedStatement.RETURN_GENERATED_KEYS);
+			
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}*/
+	} 
+	
+}
